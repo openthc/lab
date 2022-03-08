8cd3a47d03f04432baab1d46caa127bc822aa6e8
openthc <code@openthc.com>
2022-03-08 04:54:14 +0000

    add result update

---
+++ lib/Controller/Result/Update.php
@@ -0,0 +1,162 @@
+<?php
+/**
+ * Update a Lab Result
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+namespace App\Controller\Result;
+
+use Edoceo\Radix\Session;
+
+use App\Lab_Sample;
+use App\Lab_Result;
+
+class Update extends \App\Controller\Result\View
+{
+	/**
+	 *
+	 */
+	function __invoke($REQ, $RES, $ARG)
+	{
+		$id = $ARG['id'];
+		if (empty($id)) {
+			_exit_text('Invalid Request [CRU-021]', 400);
+		}
+
+		$dbc = $this->_container->DBC_User;
+
+		// $data = $this->load_lab_result_full($id);
+
+		// $chk = $dbc_user->fetchRow('SELECT * FROM lab_result WHERE (id = :lr0 OR guid = :lr0)', [ ':lr0' => $id ]);
+		$Lab_Result = new Lab_Result($dbc, $id);
+		if (empty($Lab_Result['id'])) {
+			_exit_text('WSHERE is RESULT');
+		}
+
+		$Lab_Sample = new Lab_Sample($dbc, $Lab_Result['lab_sample_id']);
+		if (empty($Lab_Sample['id'])) {
+			_exit_text('WSHERE is SAMPLE');
+		}
+
+		$data = $this->loadSiteData($data);
+		$data['Page'] = [
+			'title' => 'Result :: Update'
+		];
+
+		$data['Lab_Sample'] = $Lab_Sample->toArray();
+		$data['Lab_Result'] = $Lab_Result->toArray();
+
+		$data['Lab_Result']['coa_file'] = $Lab_Result->getCOAFile();
+		if (!is_file($data['Lab_Result']['coa_file'])) {
+			$data['Lab_Result']['coa_file'] = null;
+		}
+
+		$data['Result_Metric_Group_list'] = $Lab_Result->getMetrics_Grouped();
+
+		$data['Lot'] = $dbc->fetchRow('SELECT * FROM inventory WHERE id = :i0', [ ':i0' => $Lab_Sample['lot_id'] ]);
+		$data['Product'] = $dbc->fetchRow('SELECT * FROM product WHERE id = ?', [ $data['Lot']['product_id'] ]);
+		$data['Product_Type'] = $dbc->fetchRow('SELECT * FROM product_type WHERE id = ?', [ $data['Product']['product_type_id'] ]);
+		$data['Variety'] = $dbc->fetchRow('SELECT * FROM variety WHERE id = ?', [ $data['Lot']['variety_id'] ]);
+
+		return $RES->write( $this->render('result/update.php', $data) );
+
+	}
+
+	/**
+	 *
+	 */
+	function post($REQ, $RES, $ARG)
+	{
+		$dbc = $this->_container->DBC_User;
+
+		$LR = new Lab_Result($dbc, $ARG['id']);
+		if (empty($LR['id'])) {
+			_exit_text('Invalid Lab Result [CRU-075]', 400);
+		}
+
+		switch ($_POST['a']) {
+			case 'lab-result-delete':
+
+				$arg = [
+					':lr0' => $LR['id']
+				];
+
+				$dbc->query('BEGIN');
+
+				$dbc->query('DELETE FROM inventory_inventory_qa_sample WHERE inventory_qa_sample_id = :lr0', $arg);
+				// $dbc->query('DELETE FROM inventory_lab_result WHERE lab_result_id = :lr0', $arg);
+				$dbc->query('DELETE FROM lab_result_metric WHERE lab_result_id = :lr0', $arg);
+				$dbc->query('DELETE FROM lab_result WHERE id = :lr0', $arg);
+
+				// Reset Flags on Sample
+				$LS = new Lab_Sample($dbc, $LR['lab_sample_id']);
+
+				// Reset Flags on Inventory?
+				// $IY = new Inventory($dbc, $LS['inventory_id']);
+
+				// Alert
+
+				$dbc->query('COMMIT');
+
+				return $RES->withRedirect('/result');
+
+				break;
+
+			case 'lab-result-save':
+
+				// var_dump($_POST);
+
+				$sql = "SELECT *, meta->>'uom' AS uom FROM lab_metric";
+				$res_lab_metric = $dbc->fetchAll($sql);
+
+				foreach ($res_lab_metric as $m) {
+
+					// $lrm = new Lab_Result_Metric($dbc, [
+					// 	'lab_result_id' => $LR['id'],
+					// 	'lab_metric_id' => $m['id']
+					// ]);
+
+					$k = sprintf('lab-metric-%s', $m['id']);
+
+					$qom = trim($_POST[$k]);
+					if (strlen($qom) == 0) {
+						continue;
+					}
+
+					switch ($qom) {
+						case 'N/A':
+							$qom = -1;
+							break;
+						case 'N/D':
+							$qom = -2;
+							break;
+						case 'N/T':
+							$qom = -3;
+							break;
+					}
+
+					$sql = <<<SQL
+					INSERT INTO lab_result_metric (lab_result_id, lab_metric_id, qom, uom)
+					VALUES (:lr0, :lm0, :q0, :u0)
+					ON CONFLICT (lab_result_id, lab_metric_id)
+					DO UPDATE SET qom = :q0, uom = :u0
+					   WHERE lab_result_id = :lr0 AND lab_metric_id = :lm0
+					SQL;
+					$dbc->query($sql, [
+						// 'id' => _ulid(),
+						':lr0' => $LR['id'],
+						':lm0' => $m['id'],
+						':q0' => floatval($qom),
+						':u0' => $m['uom'],
+						// 'lod' => $m['meta']['lod'],
+						// 'loq' => $m['meta']['loq'],
+					]);
+				}
+
+				exit;
+
+		}
+
+	}
+}
---
+++ view/result/update.php
@@ -0,0 +1,325 @@
+<?php
+/**
+ * Lab Result Update
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+use Edoceo\Radix\DB\SQL;
+
+?>
+
+<h1><a href="/result">Result</a> :: Update</h1>
+
+<style>
+.lab-metric-grid {
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	/* justify-content: space-between; */
+	margin: 0 -0.25rem;
+}
+.lab-metric-item {
+	flex: 1 0 33.3333%;
+	/* max-width: 30rem; */
+	min-width: 15rem;
+	padding: 0.25rem;
+}
+.lab-metric-item .input-group-prepend .input-group-text {
+	display: block;
+	overflow: hidden;
+	text-align: left;
+	text-overflow: ellipsis;
+	white-space: nowrap;
+	width: 10em;
+}
+.lab-metric-item .input-group-prepend .input-group-text:hover {
+	min-width: 10em;
+	width: auto;
+}
+.lab-metric-item::after {
+	content: '';
+	flex: auto;
+}
+</style>
+
+<form action="" autocomplete="off" method="post">
+<div class="container">
+<section style="margin-bottom:1rem;">
+
+	<div class="row">
+		<div class="col-md-6">
+			<div class="input-group mb-2">
+				<div class="input-group-text">
+					<a href="/sample/<?= $data['Sample']['id'] ?>">Sample:</a>
+				</div>
+				<input class="form-control" readonly value="<?= __h($data['Sample']['name']) ?>">
+			</div>
+		</div>
+		<div class="col-md-6">
+			<div class="input-group mb-2">
+				<div class="input-group-text">
+					<a href="/report/license?id=<?= $data['License_Source']['id'] ?>">License:</a>
+				</div>
+				<input class="form-control" readonly value="<?= __h($data['License_Source']['name']) ?>">
+			</div>
+		</div>
+	</div>
+
+
+	<div class="row">
+		<div class="col-md-4">
+			<div class="input-group mb-2">
+				<div class="input-group-text">Lot:</div>
+				<input class="form-control" readonly value="<?= __h($data['Lot']['guid']) ?>">
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="input-group mb-2">
+				<div class="input-group-text">Product:</div>
+				<input class="form-control" readonly value="<?= __h($data['Product']['name']) ?> [<?= basename($data['Product_Type']['name']) ?>]">
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="input-group mb-2">
+				<div class="input-group-text">Variety:</div>
+				<input class="form-control" readonly value="<?= __h($data['Variety']['name']) ?>">
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php
+foreach ($data['Result_Metric_Group_list'] as $lms_id => $lms) {
+?>
+	<section style="margin-bottom:1rem;">
+
+		<div class="d-flex justify-content-between">
+			<div>
+				<h2><?= __h($lms['name']) ?></h2>
+			</div>
+			<?php
+			if ('General' != $lms['name']) {
+			?>
+				<div>
+					<div class="btn-group btn-group-sm">
+						<button class="btn btn-outline-secondary lab-metric-qom-bulk" type="button" value="OK">OK</button>
+						<button class="btn btn-outline-secondary lab-metric-qom-bulk" type="button" value="N/A">N/A</button>
+						<button class="btn btn-outline-secondary lab-metric-qom-bulk" type="button" value="N/D">N/D</button>
+						<button class="btn btn-outline-secondary lab-metric-qom-bulk" type="button" value="N/T">N/T</button>
+					</div>
+				</div>
+				<div>
+					<div class="btn-group btn-group-sm">
+						<?php
+						$uom_list = [
+							'pct' => '%',
+							'mg'  => 'mg',
+							'mgg' => 'mg/g',
+							'mgs' => 'mg/s',
+						];
+						foreach ($uom_list as $k => $v) {
+							printf('<button class="btn btn-outline-secondary lab-metric-uom-bulk" data-uom="%s" type="button">%s</button>'
+								, $k
+								, $v
+							);
+						}
+						?>
+					</div>
+				</div>
+			<?php
+			}
+			?>
+		</div>
+
+		<div class="lab-metric-grid" id="lab-matric-type-018NY6XC00LMT0BY5GND653C0C">
+		<?php
+		foreach ($lms['metric_list'] as $lm_id => $lm) {
+			switch ($lm['meta']['uom']) {
+			case 'bool':
+				_draw_metric_select_pass_fail($lm);
+				break;
+			default:
+				_draw_metric($lm);
+				break;
+			}
+		}
+		?>
+		</div>
+	</section>
+	<hr>
+<?php
+}
+?>
+
+<div class="form-actions">
+	<input name="sample_id" type="hidden" value="<?= $data['Sample']['id'] ?>">
+	<button class="btn btn-outline-primary" name="a" value="lab-result-save"><i class="fas fa-save"></i> Save</button>
+	<button class="btn btn-outline-danger" name="a" value="lab-result-delete"><i class="fas fa-save"></i> Delete</button>
+</div>
+
+</div>
+</form>
+
+
+<script>
+$(function() {
+
+	// QOM Handy Picker of All
+	$('.lab-metric-qom-bulk').on('click', function() {
+		var wrap = $(this).closest('section');
+		var sel = this.value;
+		if ('OK' == sel) {
+			wrap.find('.lab-metric-qom').val('');
+		} else {
+			wrap.find('.lab-metric-qom').val(sel);
+		}
+	});
+
+	$('.lab-metric-uom-bulk').on('click', function() {
+		debugger;
+		var wrap = $(this).closest('section');
+		var sel = this.getAttribute('data-uom');
+		wrap.find('.lab-metric-uom').val(sel);
+
+	});
+
+	// Attempt Magic on the THC Values?
+	var auto_sum = true;
+	$('#lab-matric-type-018NY6XC00LMT0BY5GND653C0C input.lab-metric-qom').on('keyup', function() {
+
+		switch (this.id) {
+			case 'lab-metric-018NY6XC00V7ACCY94MHYWNWRN':
+			case 'lab-metric-018NY6XC00PXG4PH0TXS014VVW':
+			case 'lab-metric-018NY6XC00DEEZ41QBXR2E3T97':
+			case 'lab-metric-018NY6XC00SAE8Q4JSMF40YSZ3':
+				this.style.borderColor = '#f00';
+				// this.setAttribute('data-auto-sum', '0');
+				return;
+		}
+
+		var sum_all = sum_cbd = sum_thc = 0;
+		$('#lab-matric-type-018NY6XC00LMT0BY5GND653C0C input.lab-metric-qom').each(function(i, n) {
+
+			var v = parseFloat(this.value, 10) || 0;
+			if (0 == v) {
+				return;
+			}
+
+			v = (v * 100);
+
+			switch (n.id) {
+				case 'lab-metric-018NY6XC00V7ACCY94MHYWNWRN':
+				case 'lab-metric-018NY6XC00PXG4PH0TXS014VVW':
+				case 'lab-metric-018NY6XC00DEEZ41QBXR2E3T97':
+				case 'lab-metric-018NY6XC00SAE8Q4JSMF40YSZ3':
+					return; // Ignore These
+					break;
+				case 'lab-metric-018NY6XC00LM49CV7QP9KM9QH9': // d9-thc
+					sum_thc += v;
+					break;
+				case 'lab-metric-018NY6XC00LMB0JPRM2SF8F9F2': // d9-thca
+					sum_thc += (v * 0.877);
+					break;
+				case 'lab-metric-018NY6XC00LM877GAKMFPK7BMC': // d8-thc ??
+					// nothing for now
+					break;
+				case 'lab-metric-018NY6XC00LMK7KHD3HPW0Y90N': // cbd
+					sum_cbd += v;
+					break;
+				case 'lab-metric-018NY6XC00LMENDHEH2Y32X903': // cbda
+					sum_cbd += (v * 0.877);
+					break;
+			}
+
+			sum_all += v;
+
+		});
+
+		sum_all = sum_all / 100;
+		sum_cbd = sum_cbd / 100;
+		sum_thc = sum_thc / 100;
+
+		$('#lab-metric-018NY6XC00V7ACCY94MHYWNWRN').val((sum_cbd + sum_thc).toFixed(3));
+		$('#lab-metric-018NY6XC00PXG4PH0TXS014VVW').val(sum_thc.toFixed(3));
+		$('#lab-metric-018NY6XC00DEEZ41QBXR2E3T97').val(sum_cbd.toFixed(3));
+		$('#lab-metric-018NY6XC00SAE8Q4JSMF40YSZ3').val(sum_all.toFixed(3));
+
+
+	});
+
+});
+</script>
+
+
+<?php
+
+
+/**
+ *
+ */
+function _draw_metric($lm)
+{
+	$uom_list = [
+		'pct' => '%',
+		'mg'  => 'mg',
+		'mgg' => 'mg/g',
+		'mgs' => 'mg/s',
+	];
+
+?>
+	<div class="lab-metric-item">
+		<div class="input-group">
+			<div class="input-group-prepend">
+				<div class="input-group-text"><?= __h($lm['name']) ?></div>
+			</div>
+			<input
+				autocomplete="off"
+				class="form-control r lab-metric-qom"
+				data-auto-sum="1"
+				id="<?= sprintf('lab-metric-%s', $lm['id']) ?>"
+				name="<?= sprintf('lab-metric-%s', $lm['id']) ?>"
+				placeholder="<?= __h($lm['name']) ?>"
+				value="<?= $lm['metric']['qom'] ?>">
+			<select
+				class="form-control lab-metric-uom"
+				name="<?= sprintf('lab-metric-%s-uom', $lm['id']) ?>"
+				style="flex: 0 1 5em; width: 5em;"
+				tabindex="-1">
+			<?php
+			foreach ($uom_list as $v => $n) {
+				$sel = ($v == $lm['metric']['uom'] ? ' selected' : null);
+				printf('<option%s value="%s">%s</option>', $sel, $v, $n);
+			}
+			?>
+			</select>
+		</div>
+	</div>
+<?php
+}
+
+
+
+/**
+ *
+ */
+function _draw_metric_select_pass_fail($lm)
+{
+	// $sel =  == 1 ? 'pass' : 'fail';
+	$sel = $lm['metric']['qom'];
+
+?>
+	<div class="lab-metric-item">
+		<div class="input-group">
+			<div class="input-group-prepend">
+				<div class="input-group-text"><?= __h($lm['name']) ?></div>
+			</div>
+			<select class="form-control" name="<?= sprintf('lab-metric-%s', $lm['id']) ?>">
+				<option <?= ($sel == '-1' ? 'selected' : null) ?> value="-1">n/a</option>
+				<option <?= ($sel == '0' ? 'selected' : null) ?> value="0">Fail</option>
+				<option <?= ($sel == '1' ? 'selected' : null) ?> value="1">Pass</option>
+			</select>
+		</div>
+	</div>
+<?php
+}
