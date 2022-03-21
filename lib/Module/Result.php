0cff62b7aadcbfa30b579f624a015b3dc0dcd477
openthc <code@openthc.com>
2022-03-21 03:21:13 +0000

    update Routes

--- lib/Module/Result.php
+++ lib/Module/Result.php
@@ -1,6 +1,8 @@
 <?php
 /**
  * Wraps all the Routing for the Result Module
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
  */
 
 namespace App\Module;
@@ -13,17 +15,21 @@ class Result extends \OpenTHC\Module\Base
 	{
 		$a->get('', 'App\Controller\Result\Home');
 
+		// $a->map(['GET','POST'], '/sync', 'App\Controller\Result\Sync');
+		// $a->map(['GET','POST'], '/{id}/sync', 'App\Controller\Result\Sync');
+
 		$a->get('/create', 'App\Controller\Result\Create');
-		$a->post('/create/save', 'App\Controller\Result\Create:save');
+		$a->post('/create', 'App\Controller\Result\Create:save');
 
 		$a->get('/download', 'App\Controller\Result\Download');
 		$a->map(['GET','POST'], '/upload', 'App\Controller\Result\Upload');
 		$a->get('/upload/preview', 'App\Controller\Result\Upload:preview');
 		$a->map(['GET','POST'], '/upload/queue', 'App\Controller\Result\Queue');
 
-		$a->get('/edit', 'App\Controller\Result\Edit');
-
 		$a->map([ 'GET', 'POST'], '/{id}', 'App\Controller\Result\View');
+
+		$a->get('/{id}/update', 'App\Controller\Result\Update');
+		$a->post('/{id}/update', 'App\Controller\Result\Update:post');
 
 	}
 }
