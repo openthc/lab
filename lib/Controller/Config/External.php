9922e2f9258245bfe58e7ded5c43b527b319f872
openthc <code@openthc.com>
2022-04-12 02:22:13 +0000

    update external config

--- lib/Controller/Config/External.php
+++ lib/Controller/Config/External.php
@@ -7,21 +7,24 @@ namespace App\Controller\Config;
 
 class External extends \App\Controller\Base
 {
+	const CONFIG_KEY = 'qbench-auth';
+
 	function __invoke($REQ, $RES, $ARG)
 	{
 		$data = $this->loadSiteData();
 		$data['Page']['title'] = 'Config :: External';
 
+		$dbc = $this->_container->DBC_User;
+
 		// Save iT?
 		switch ($_POST['a']) {
 			case 'qbench-save':
-				$dbc = $this->_container->DBC_User;
 
-				$chk = $dbc->fetchOne('SELECT id FROM base_option WHERE key = :k', [ ':k' => 'qbench-api' ]);
+				$chk = $dbc->fetchOne('SELECT id FROM base_option WHERE key = :k', [ ':k' => self::CONFIG_KEY ]);
 				if (empty($chk)) {
 					$dbc->insert('base_option', [
 						'id' => _ulid(),
-						'key' => 'qbench-api',
+						'key' => self::CONFIG_KEY,
 						'val' => json_encode([
 							'server-url' => $_POST['qbench-server-url']
 							, 'public-key' => $_POST['qbench-public-key']
@@ -43,6 +46,14 @@ class External extends \App\Controller\Base
 				return $RES->withRedirect('/config');
 
 		}
+
+		$cfg = $dbc->fetchOne('SELECT val FROM base_option WHERE key = :k', [ ':k' => self::CONFIG_KEY ]);
+		$cfg = json_decode($cfg, true);
+		$data = [
+			'qbench-server-url' => $cfg['server-url'],
+			'qbench-public-key' => $cfg['public-key'],
+			'qbench-secret-key' => $cfg['secret-key'],
+		];
 
 		return $RES->write( $this->render('config/external.php', $data) );
 
--- view/config/external.php
+++ view/config/external.php
@@ -5,17 +5,17 @@
 
 	<div class="mb-2">
 		<label>QBench API Endpoint</label>
-		<input class="form-control" name="qbench-server-url" value="https://..." >
+		<input class="form-control" name="qbench-server-url" value="<?= __h($data['qbench-server-url']) ?>">
 	</div>
 
 	<div class="mb-2">
 		<label>QBench Public Key</label>
-		<input class="form-control" name="qbench-public-key" value="">
+		<input class="form-control" name="qbench-public-key" value="<?= __h($data['qbench-public-key']) ?>">
 	</div>
 
 	<div class="mb-2">
 		<label>QBench Secret Key</label>
-		<input class="form-control" name="qbench-secret-key" value="">
+		<input class="form-control" name="qbench-secret-key" value="<?= __h($data['qbench-secret-key']) ?>">
 	</div>
 
 	<div>
