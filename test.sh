693fe83ea3b382a31e2088f2cb9e0b7146505e68
openthc <code@openthc.com>
2022-04-27 04:27:08 +0000

    add test runner

---
+++ test.sh
@@ -0,0 +1,121 @@
+#!/bin/bash -x
+#
+# OpenTHC Test Runner
+#
+
+set -o errexit
+set -o nounset
+
+f=$(readlink -f "$0")
+d=$(dirname "$f")
+
+cd "$d"
+
+output_base="webroot/test-output"
+output_main="$output_base/index.html"
+mkdir -p "$output_base"
+
+
+#
+# Lint
+if [ ! -f "$output_base/phplint.txt" ]
+then
+
+	echo '<h1>Linting...</h1>' > "$output_main"
+
+	find bin/ lib/ view/ -type f -name '*.php' -exec php -l {} \; \
+		| grep -v 'No syntax' || true \
+		2>&1 \
+		>"$output_base/phplint.txt"
+
+	[ -s "$output_base/phplint.txt" ] || echo "Linting OK" >"$output_base/phplint.txt"
+
+fi
+
+
+#
+# PHP-CPD
+if [ ! -f "$output_base/phpcpd.txt" ]
+then
+
+	echo '<h1>CPD Check</h1>' > "$output_main"
+
+	vendor/bin/phpcpd \
+		--fuzzy \
+		$code_list \
+		2>&1 \
+		> "$output_base/phpcpd.txt"
+
+fi
+
+
+#
+# PHPStan
+if [ ! -f "$output_base/phpstan.html" ]
+then
+	echo '<h1>PHPStan...</h1>' > "$output_main"
+	vendor/bin/phpstan analyze --error-format=junit --no-progress > "$output_base/phpstan.xml" || true
+	[ -f "phpstan.xsl" ] || curl -qs 'https://openthc.com/pub/phpstan.xsl' > "test/phpstan.xsl"
+	xsltproc \
+		--nomkdir \
+		--output "$output_base/phpstan.html" \
+		phpstan.xsl \
+		"$output_base/phpstan.xml"
+fi
+
+
+#
+# PHPUnit
+echo '<h1>Running Tests...</h1>' > "$output_main"
+vendor/bin/phpunit \
+	--verbose \
+	--log-junit "$output_base/output.xml" \
+	--testdox-html "$output_base/testdox.html" \
+	--testdox-text "$output_base/testdox.txt" \
+	--testdox-xml "$output_base/testdox.xml" \
+	"$@" 2>&1 | tee "$output_base/output.txt"
+
+
+#
+# Transform
+echo '<h1>Transforming...</h1>' > "$output_main"
+[ -f "report.xsl" ] || wget -q 'https://openthc.com/pub/phpunit/report.xsl'
+xsltproc \
+	--nomkdir \
+	--output "$output_base/output.html" \
+	report.xsl \
+	"$output_base/output.xml"
+
+
+#
+# Final Output
+dt=$(date)
+note=$(tail -n1 "$output_base/output.txt")
+
+cat <<HTML > "$output_main"
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="initial-scale=1, user-scalable=yes">
+<meta name="theme-color" content="#247420">
+<link rel="stylesheet" href="https://cdn.openthc.com/bootstrap/4.4.1/bootstrap.css" integrity="sha256-L/W5Wfqfa0sdBNIKN9cG6QA5F2qx4qICmU2VgLruv9Y=" crossorigin="anonymous">
+<title>Test Result ${dt}</title>
+</head>
+<body>
+<div class="container mt-4">
+<div class="jumbotron">
+
+<h1>Test Result ${dt}</h1>
+<h2>${note}</h2>
+
+<p>You can view the <a href="output.txt">raw script output</a>,
+or the <a href="output.xml">Unit Test XML</a>
+which we've processed <small>(via XSL)</small> to <a href="output.html">a pretty report</a>
+which is also in <a href="testdox.html">testdox format</a>.
+</p>
+
+</div>
+</div>
+</body>
+</html>
+HTML
