6433921efad2091fe9b1633cbffe57a66e44b5b4
openthc <code@openthc.com>
2022-05-02 02:33:41 +0000

    update CCRS output

--- view/result/csv-ccrs.php
+++ view/result/csv-ccrs.php
@@ -9,11 +9,9 @@
 
 use OpenTHC\CRE\CCRS;
 
-header('content-type: text/plain');
+use App\Lab_Result;
 
-// @hack
-$data['License_Origin']['code'] = 'SRC_LIC_CODE';
-$data['Laboratory_License']['code'] = 'LAB_LIC_CODE';
+header('content-type: text/plain');
 
 $dt0 = new DateTime($data['Lab_Result']['created_at']);
 
@@ -48,83 +46,82 @@ $out_metric_list['018NY6XC00LM10ZPAN42R490W3'] = 'Heavy Metal - Mercury (ug/g) H
 
 
 // Solvent
-// Residual Solvent - Acetone (ug/g) Residual Solvent - Acetone (ppm)
-// Residual Solvent - Benzene (ug/g) Residual Solvent - Benzene (ppm)
-// Residual Solvent - Butanes (ug/g) Residual Solvent - Butanes (ppm)
-// Residual Solvent - Cyclohexane (ug/g) Residual Solvent - Cyclohexane (ppm)
-// Residual Solvent - Chloroform (ug/g) Residual Solvent - Chloroform (ppm)
-// Residual Solvent - Dichloromethane (ug/g) Residual Solvent - Dichloromethane (ppm)
-// Residual Solvent - Ethyl Acetate (ug/g) Residual Solvent - Ethyl Acetate (ppm)
-// Residual Solvent - Heptanes (ug/g) Residual Solvent - Heptanes (ppm)
-// Residual Solvent - Hexanes (ug/g) Residual Solvent - Hexanes (ppm)
-// Residual Solvent - Isopropanol (ug/g) Residual Solvent - Isopropanol (ppm)
-// Residual Solvent - Methanol (ug/g) Residual Solvent - Methanol (ppm)
-// Residual Solvent - Pentanes (ug/g) Residual Solvent - Pentanes (ppm)
-// Residual Solvent - Propanes (ug/g) Residual Solvent - Propanes (ppm)
-// Residual Solvent - Toluene (ug/g) Residual Solvent - Toluene (ppm)
-// Residual Solvent - Xylenes (ug/g) Residual Solvent - Xylenes (ppm)
+$out_metric_list['018NY6XC00LM9HW5DZGD5KR55G'] = 'Residual Solvent - Acetone (ug/g) Residual Solvent - Acetone (ppm)';
+$out_metric_list['018NY6XC00LMT7VRMWMXMH59Y5'] = 'Residual Solvent - Benzene (ug/g) Residual Solvent - Benzene (ppm)';
+$out_metric_list['018NY6XC00LMSTBW55VFR0QG56'] = 'Residual Solvent - Butanes (ug/g) Residual Solvent - Butanes (ppm)';
+$out_metric_list['018NY6XC00LM6QHYX79AXVVRH1'] = 'Residual Solvent - Cyclohexane (ug/g) Residual Solvent - Cyclohexane (ppm)';
+$out_metric_list['018NY6XC00LMAA8QXZ8CD0QQMW'] = 'Residual Solvent - Chloroform (ug/g) Residual Solvent - Chloroform (ppm)';
+$out_metric_list['018NY6XC00LM7D70QAKTR6WM30'] = 'Residual Solvent - Dichloromethane (ug/g) Residual Solvent - Dichloromethane (ppm)';
+$out_metric_list['018NY6XC00LMH5RPYTRCS5BQKJ'] = 'Residual Solvent - Ethyl Acetate (ug/g) Residual Solvent - Ethyl Acetate (ppm)';
+$out_metric_list['018NY6XC00LM50MYZZY71MQ7BE'] = 'Residual Solvent - Heptanes (ug/g) Residual Solvent - Heptanes (ppm)';
+$out_metric_list['018NY6XC00LM7EC335XECKPV3X'] = 'Residual Solvent - Hexanes (ug/g) Residual Solvent - Hexanes (ppm)';
+$out_metric_list['018NY6XC00LM3CKFFVVSNJYGCH'] = 'Residual Solvent - Isopropanol (ug/g) Residual Solvent - Isopropanol (ppm)';
+$out_metric_list['018NY6XC00LMYC6MEJARSBRGW8'] = 'Residual Solvent - Methanol (ug/g) Residual Solvent - Methanol (ppm)';
+$out_metric_list['018NY6XC00LM68678PK1SAVVR5'] = 'Residual Solvent - Pentanes (ug/g) Residual Solvent - Pentanes (ppm)';
+$out_metric_list['018NY6XC00LMCK0YZ3T76QWMNF'] = 'Residual Solvent - Propanes (ug/g) Residual Solvent - Propanes (ppm)';
+$out_metric_list['018NY6XC00LMGG9JR3SM0MEDGQ'] = 'Residual Solvent - Toluene (ug/g) Residual Solvent - Toluene (ppm)';
+$out_metric_list['018NY6XC00LMW1FC0RA14FZ3PF'] = 'Residual Solvent - Xylenes (ug/g) Residual Solvent - Xylenes (ppm)';
 
 // Pesticide
-// Pesticide - Abamectin (ug/g) Pesticide - Abamectin (ppm)
-// Pesticide - Acephate (ug/g) Pesticide - Acephate (ppm)
-// Pesticide - Acequinocyl (ug/g) Pesticide - Acequinocyl (ppm)
-// Pesticide - Acetamiprid (ug/g) Pesticide - Acetamiprid (ppm)
-// Pesticide - Aldicarb (ug/g) Pesticide - Aldicarb (ppm)
-// Pesticide - Azoxystrobin (ug/g) Pesticide - Azoxystrobin (ppm)
-// Pesticide - Bifenazate (ug/g) Pesticide - Bifenazate (ppm)
-// Pesticide - Bifenthrin (ug/g) Pesticide - Bifenthrin (ppm)
-// Pesticide - Boscalid (ug/g) Pesticide - Boscalid (ppm)
-// Pesticide - Carbaryl (ug/g) Pesticide - Carbaryl (ppm)
-// Pesticide - Carbofuran (ug/g) Pesticide - Carbofuran (ppm)
-// Pesticide - Chlorantraniliprole (ug/g) Pesticide - Chlorantraniliprole (ppm)
-// Pesticide - Chlorfenapyr (ug/g) Pesticide - Chlorfenapyr (ppm)
-// Pesticide - Chlorpyrifos (ug/g) Pesticide - Chlorpyrifos (ppm)
-// Pesticide - Clofentizine (ug/g) Pesticide - Clofentizine (ppm)
-// Pesticide - Cyfluthrin (ug/g) Pesticide - Cyfluthrin (ppm)
-// Pesticide - Cypermethrin (ug/g) Pesticide - Cypermethrin (ppm)
-// Pesticide - Daminozide (ug/g) Pesticide - Daminozide (ppm)
-// Pesticide - DDVP (dichlorvos) (ug/g) Pesticide - DDVP (dichlorvos) (ppm)
-// Pesticide - Diazinon (ug/g) Pesticide - Diazinon (ppm)
-// Pesticide - Dimethoate (ug/g) Pesticide - Dimethoate (ppm)
-// Pesticide - Ethoprophos (ug/g) Pesticide - Ethoprophos (ppm)
-// Pesticide - Etofenprox (ug/g) Pesticide - Etofenprox (ppm)
-// Pesticide - Etoxazole (ug/g) Pesticide - Etoxazole (ppm)
-// Pesticide - Fenoxycarb (ug/g) Pesticide - Fenoxycarb (ppm)
-// Pesticide - Fenpyroximate (ug/g) Pesticide - Fenpyroximate (ppm)
-// Pesticide - Fipronil (ug/g) Pesticide - Fipronil (ppm)
-// Pesticide - Flonicamid (ug/g) Pesticide - Flonicamid (ppm)
-// Pesticide - Fludioxonil (ug/g) Pesticide - Fludioxonil (ppm)
-// Pesticide - Hexythiazox (ug/g) Pesticide - Hexythiazox (ppm)
-// Pesticide - Imazalil (ug/g) Pesticide - Imazalil (ppm)
-// Pesticide - Imidacloprid (ug/g) Pesticide - Imidacloprid (ppm)
-// Pesticide - Kresoxim-Methyl (ug/g) Pesticide - Kresoxim-Methyl (ppm)
-// Pesticide - Malathion (ug/g) Pesticide - Malathion (ppm)
-// Pesticide - Metalaxyl (ug/g) Pesticide - Metalaxyl (ppm)
-// Pesticide - Methiocarb (ug/g) Pesticide - Methiocarb (ppm)
-// Pesticide - Methomyl (ug/g) Pesticide - Methomyl (ppm)
-// Pesticide - Methyl parathion (ug/g) Pesticide - Methyl parathion (ppm)
-// Pesticide - MGK-264 (ug/g) Pesticide - MGK-264 (ppm)
-// Pesticide - Myclobutanil (ug/g) Pesticide - Myclobutanil (ppm)
-// Pesticide - Naled (ug/g) Pesticide - Naled (ppm)
-// Pesticide - Oxamyl (ug/g) Pesticide - Oxamyl (ppm)
-// Pesticide - Paclobutrazol (ug/g) Pesticide - Paclobutrazol (ppm)
-// Pesticide - Permethrins (ug/g) Pesticide - Permethrins (ppm)
-// Pesticide - Phosmet (ug/g) Pesticide - Phosmet (ppm)
-// Pesticide - Piperonyl butoxide (ug/g) Pesticide - Piperonyl butoxide (ppm)
-// Pesticide - Prallethrin (ug/g) Pesticide - Prallethrin (ppm)
-// Pesticide - Propiconazole (ug/g) Pesticide - Propiconazole (ppm)
-// Pesticide - Propoxur (ug/g) Pesticide - Propoxur (ppm)
-// Pesticide - Pyrethrins (ug/g) Pesticide - Pyrethrins (ppm)
-// Pesticide - Pyridaben (ug/g) Pesticide - Pyridaben (ppm)
-// Pesticide - Spinosad (ug/g) Pesticide - Spinosad (ppm)
-// Pesticide - Spiromesifen (ug/g) Pesticide - Spiromesifen (ppm)
-// Pesticide - Spirotetramat (ug/g) Pesticide - Spirotetramat (ppm)
-// Pesticide - Spiroxamine (ug/g) Pesticide - Spiroxamine (ppm)
-// Pesticide - Tebuconazole (ug/g) Pesticide - Tebuconazole (ppm)
-// Pesticide - Thiacloprid (ug/g) Pesticide - Thiacloprid (ppm)
-// Pesticide - Thiamethoxam (ug/g) Pesticide - Thiamethoxam (ppm)
-// Pesticide - Trifloxystrobin (ug/g) Pesticide - Trifloxystrobin (ppm)
-
+$out_metric_list['018NY6XC00LMGZM1K01HQG6A04'] = 'Pesticide - Abamectin (ug/g) Pesticide - Abamectin (ppm)';
+$out_metric_list['018NY6XC00LMME2KAJD5CJZCFC'] = 'Pesticide - Acephate (ug/g) Pesticide - Acephate (ppm)';
+$out_metric_list['018NY6XC00LMB7TEPP64SS0VXD'] = 'Pesticide - Acequinocyl (ug/g) Pesticide - Acequinocyl (ppm)';
+$out_metric_list['018NY6XC00LMHPJYYDQT7FCM8P'] = 'Pesticide - Acetamiprid (ug/g) Pesticide - Acetamiprid (ppm)';
+$out_metric_list['018NY6XC00LME4KJM6Y8XP8WGA'] = 'Pesticide - Aldicarb (ug/g) Pesticide - Aldicarb (ppm)';
+$out_metric_list['018NY6XC00LMK2VVW868567PHD'] = 'Pesticide - Azoxystrobin (ug/g) Pesticide - Azoxystrobin (ppm)';
+$out_metric_list['018NY6XC00LMKCE4E30P3R72SK'] = 'Pesticide - Bifenazate (ug/g) Pesticide - Bifenazate (ppm)';
+$out_metric_list['018NY6XC00LMPH4K88KC1PKJVJ'] = 'Pesticide - Bifenthrin (ug/g) Pesticide - Bifenthrin (ppm)';
+$out_metric_list['018NY6XC00LM3P767WQ0KSFARZ'] = 'Pesticide - Boscalid (ug/g) Pesticide - Boscalid (ppm)';
+$out_metric_list['018NY6XC00LMZP42VJGA642TEB'] = 'Pesticide - Carbaryl (ug/g) Pesticide - Carbaryl (ppm)';
+$out_metric_list['018NY6XC00LM7N4CCX5ZRVADDN'] = 'Pesticide - Carbofuran (ug/g) Pesticide - Carbofuran (ppm)';
+$out_metric_list['018NY6XC00LMKKEFRB9BJ8KP0P'] = 'Pesticide - Chlorantraniliprole (ug/g) Pesticide - Chlorantraniliprole (ppm)';
+$out_metric_list['018NY6XC00LMR0FZPVYAEJ8JET'] = 'Pesticide - Chlorfenapyr (ug/g) Pesticide - Chlorfenapyr (ppm)';
+$out_metric_list['018NY6XC00LMXCM3VG0XGR6KAH'] = 'Pesticide - Chlorpyrifos (ug/g) Pesticide - Chlorpyrifos (ppm)';
+$out_metric_list['018NY6XC00LM230ECKQSRFZ2BE'] = 'Pesticide - Clofentizine (ug/g) Pesticide - Clofentizine (ppm)';
+$out_metric_list['018NY6XC00LMCQDJ36Y13GX6W3'] = 'Pesticide - Cyfluthrin (ug/g) Pesticide - Cyfluthrin (ppm)';
+$out_metric_list['018NY6XC00LM7CX800BM0FSGJR'] = 'Pesticide - Cypermethrin (ug/g) Pesticide - Cypermethrin (ppm)';
+$out_metric_list['018NY6XC00LM8H6MET0WJ2YCV1'] = 'Pesticide - Daminozide (ug/g) Pesticide - Daminozide (ppm)';
+$out_metric_list['018NY6XC00LMXVVJP95SCEWYMJ'] = 'Pesticide - DDVP (dichlorvos) (ug/g) Pesticide - DDVP (dichlorvos) (ppm)';
+$out_metric_list['018NY6XC00LM1FYB8674M2X435'] = 'Pesticide - Diazinon (ug/g) Pesticide - Diazinon (ppm)';
+$out_metric_list['018NY6XC00LMMGQYZ01JNTDSZ1'] = 'Pesticide - Dimethoate (ug/g) Pesticide - Dimethoate (ppm)';
+$out_metric_list['018NY6XC00LMYBVS4P9WE8MT73'] = 'Pesticide - Ethoprophos (ug/g) Pesticide - Ethoprophos (ppm)';
+$out_metric_list['018NY6XC00LMJHEQ07C7YKJBFM'] = 'Pesticide - Etofenprox (ug/g) Pesticide - Etofenprox (ppm)';
+$out_metric_list['018NY6XC00LMNPCTGHS6PVWKS3'] = 'Pesticide - Etoxazole (ug/g) Pesticide - Etoxazole (ppm)';
+$out_metric_list['018NY6XC00LMGN496XNG04YCKA'] = 'Pesticide - Fenoxycarb (ug/g) Pesticide - Fenoxycarb (ppm)';
+$out_metric_list['018NY6XC00LM1NGMNDNYD3R0HE'] = 'Pesticide - Fenpyroximate (ug/g) Pesticide - Fenpyroximate (ppm)';
+$out_metric_list['018NY6XC00LM98WRGGCSFYYGVX'] = 'Pesticide - Fipronil (ug/g) Pesticide - Fipronil (ppm)';
+$out_metric_list['018NY6XC00LMZMT6NYFV6QM9JH'] = 'Pesticide - Flonicamid (ug/g) Pesticide - Flonicamid (ppm)';
+$out_metric_list['018NY6XC00LMZ91MVJB81J4JQT'] = 'Pesticide - Fludioxonil (ug/g) Pesticide - Fludioxonil (ppm)';
+$out_metric_list['018NY6XC00LMADJX0GMMS5MXVB'] = 'Pesticide - Hexythiazox (ug/g) Pesticide - Hexythiazox (ppm)';
+$out_metric_list['018NY6XC00LMX1R3RFFRFZS8T4'] = 'Pesticide - Imazalil (ug/g) Pesticide - Imazalil (ppm)';
+$out_metric_list['018NY6XC00LMR9Z32S7WHPBZP9'] = 'Pesticide - Imidacloprid (ug/g) Pesticide - Imidacloprid (ppm)';
+$out_metric_list['018NY6XC00LM4VRHKTYTJJRDPW'] = 'Pesticide - Kresoxim-Methyl (ug/g) Pesticide - Kresoxim-Methyl (ppm)';
+$out_metric_list['018NY6XC00LMEN8F7VNXYV7HCS'] = 'Pesticide - Malathion (ug/g) Pesticide - Malathion (ppm)';
+$out_metric_list['018NY6XC00LMMFPYJ25XC5QTTQ'] = 'Pesticide - Metalaxyl (ug/g) Pesticide - Metalaxyl (ppm)';
+$out_metric_list['018NY6XC00LMC4048KGG4SR6WF'] = 'Pesticide - Methiocarb (ug/g) Pesticide - Methiocarb (ppm)';
+$out_metric_list['018NY6XC00LM7WBZ76X1E3T868'] = 'Pesticide - Methomyl (ug/g) Pesticide - Methomyl (ppm)';
+$out_metric_list[''] = 'Pesticide - Methyl parathion (ug/g) Pesticide - Methyl parathion (ppm)';
+$out_metric_list['018NY6XC00LMCQ7DX02S94RMM7'] = 'Pesticide - MGK-264 (ug/g) Pesticide - MGK-264 (ppm)';
+$out_metric_list['018NY6XC00LMN56HSR1X5ACEJB'] = 'Pesticide - Myclobutanil (ug/g) Pesticide - Myclobutanil (ppm)';
+$out_metric_list['018NY6XC00LMSCF0SS8VVJ9DE5'] = 'Pesticide - Naled (ug/g) Pesticide - Naled (ppm)';
+$out_metric_list['018NY6XC00LM83VNPJMHTKX5F0'] = 'Pesticide - Oxamyl (ug/g) Pesticide - Oxamyl (ppm)';
+$out_metric_list['018NY6XC00LMV3YF9F83621G84'] = 'Pesticide - Paclobutrazol (ug/g) Pesticide - Paclobutrazol (ppm)';
+$out_metric_list['018NY6XC00LM3ZJH23WAKV7JEB'] = 'Pesticide - Permethrins (ug/g) Pesticide - Permethrins (ppm)';
+$out_metric_list['018NY6XC00LMZ95MW0N3JPZ056'] = 'Pesticide - Phosmet (ug/g) Pesticide - Phosmet (ppm)';
+$out_metric_list['018NY6XC00LM6VF2D0V998AY9Q'] = 'Pesticide - Piperonyl butoxide (ug/g) Pesticide - Piperonyl butoxide (ppm)';
+$out_metric_list['018NY6XC00LMKX28NVG7PJT5WJ'] = 'Pesticide - Prallethrin (ug/g) Pesticide - Prallethrin (ppm)';
+$out_metric_list['018NY6XC00LM6T0NCQGXBCSNCS'] = 'Pesticide - Propiconazole (ug/g) Pesticide - Propiconazole (ppm)';
+$out_metric_list['018NY6XC00LMD2VKZ8FHZ3F3X8'] = 'Pesticide - Propoxur (ug/g) Pesticide - Propoxur (ppm)';
+$out_metric_list['018NY6XC00LMWSMH35NX5PQQKT'] = 'Pesticide - Pyrethrins (ug/g) Pesticide - Pyrethrins (ppm)';
+$out_metric_list['018NY6XC00LMH66XZD64ZDTHZW'] = 'Pesticide - Pyridaben (ug/g) Pesticide - Pyridaben (ppm)';
+$out_metric_list['018NY6XC00LMKF9QEXJGS0HGHM'] = 'Pesticide - Spinosad (ug/g) Pesticide - Spinosad (ppm)';
+$out_metric_list['018NY6XC00LMT9BF2M636RZBZX'] = 'Pesticide - Spiromesifen (ug/g) Pesticide - Spiromesifen (ppm)';
+$out_metric_list['018NY6XC00LMWDVDHEYRS6058S'] = 'Pesticide - Spirotetramat (ug/g) Pesticide - Spirotetramat (ppm)';
+$out_metric_list['018NY6XC00LMQ6AM5TE0FYPN2R'] = 'Pesticide - Spiroxamine (ug/g) Pesticide - Spiroxamine (ppm)';
+$out_metric_list['018NY6XC00LMT8QJD3BG6CNXA8'] = 'Pesticide - Tebuconazole (ug/g) Pesticide - Tebuconazole (ppm)';
+$out_metric_list['018NY6XC00LMMFQB5HBHJBQ9BS'] = 'Pesticide - Thiacloprid (ug/g) Pesticide - Thiacloprid (ppm)';
+$out_metric_list['018NY6XC00LMCH7YXS32M4PNZF'] = 'Pesticide - Thiamethoxam (ug/g) Pesticide - Thiamethoxam (ppm)';
+$out_metric_list['018NY6XC00LMRG0A40VCNVW3YX'] = 'Pesticide - Trifloxystrobin (ug/g) Pesticide - Trifloxystrobin (ppm)';
 
 // Build Output
 $csv_output = [];
@@ -133,10 +130,10 @@ foreach ($out_metric_list as $mk0 => $mn0) {
 	$lrm = $data['Lab_Result_Metric_list'][$mk0];
 
 	$csv_output[] = [
-		$data['License_Origin']['code'], // License Owner
+		$data['License_Source']['code'], // License Owner
 		$data['Lot']['guid'],
-		$data['Laboratory_License']['code'], // Code of the Laboratory
-		'Pass',
+		$data['License_Laboratory']['code'], // Code of the Laboratory
+		'PASS',
 		$mn0, // trim(sprintf('%s %s', $lrm['name'], $lrm['uom'])),
 		$dt0->format('Y-m-d'), // Test Date
 		$lrm['qom'],
