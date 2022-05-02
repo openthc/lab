c7a2026ebf36d3e8ed1ddef77f456b9fa3922a8e
openthc <code@openthc.com>
2022-05-02 01:17:30 +0000

    move ccrs downloader

--- lib/Controller/Result/Download.php
+++ lib/Controller/Result/Download.php
@@ -1,16 +1,21 @@
 <?php
 /**
-
-*/
+ * Generate Downloadable Types for the Files?
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
 
 namespace App\Controller\Result;
 
-class Download extends \OpenTHC\Controller\Base
+class Download extends \App\Controller\Result\View
 {
 	private $_fh;
 	private $_eol = "\r\n";
 	private $_sep = ',';
 
+	/**
+	 *
+	 */
 	function __construct($c)
 	{
 		parent::__construct($c);
@@ -42,34 +47,145 @@ class Download extends \OpenTHC\Controller\Base
 
 	}
 
+	/**
+	 *
+	 */
 	function __invoke($REQ, $RES, $ARG)
 	{
+		switch ($_GET['f']) {
+			case 'csv':
+				return $this->csv($RES, $ARG);
+			case 'csv+ccrs':
+			case 'csv ccrs':
+				return $this->csv_ccrs($RES, $ARG);
+			case 'json':
+				return $this->json($RES, $ARG);
+			case 'json+wica':
+			case 'json wica':
+				return $this->json_wcia($RES, $ARG);
+			case 'pdf':
+				return $this->pdf($RES, $ARG);
+			case 'png':
+				if ($_GET['d'] == 'coa') {
+					return $this->png_coa($RES, $ARG);
+				}
+				return $this->png($RES, $ARG);
 
-		$cre = new \OpenTHC\CRE($_SESSION['pipe-token']);
-		$res = $cre->get('/qa');
-		$res = $res['result'];
-
-		// Headers
-		//header('Cache-Control: must-revalidate');
-		//header('Content-Description: Data Download');
-		//header(sprintf('Content-Disposition: attachment; filename="%s"', $name));
-		//header('Content-Transfer-Encoding: binary');
-		//header('Content-Type: application/octet-stream');
-		//header('Content-Type: text/csv', true);
-		header('Content-Type: text/plain');
-
-		$this->_fh = tmpfile();
-		$this->_output_header($res[0]);
-
-		foreach ($res as $rec) {
-			$this->_output_record($rec);
 		}
 
-		fseek($this->_fh, 0, SEEK_SET);
-		fpassthru($this->_fh);
+		var_dump($_GET);
 
+		exit;
+
+		// $cre = new \OpenTHC\CRE($_SESSION['pipe-token']);
+		// $res = $cre->get('/qa');
+		// $res = $res['result'];
+
+		// // Headers
+		// //header('Cache-Control: must-revalidate');
+		// //header('Content-Description: Data Download');
+		// //header(sprintf('Content-Disposition: attachment; filename="%s"', $name));
+		// //header('Content-Transfer-Encoding: binary');
+		// //header('Content-Type: application/octet-stream');
+		// //header('Content-Type: text/csv', true);
+		// header('Content-Type: text/plain');
+
+		// $this->_fh = tmpfile();
+		// $this->_output_header($res[0]);
+
+		// foreach ($res as $rec) {
+		// 	$this->_output_record($rec);
+		// }
+
+		// fseek($this->_fh, 0, SEEK_SET);
+		// fpassthru($this->_fh);
+
+		// exit(0);
+	}
+
+	/**
+	 *
+	 */
+	function csv($RES, $ARG)
+	{
+		__exit_text('Dump as CSV', 501);
+	}
+
+	/**
+	 * Dump as CCRS CSV File
+	 */
+	function csv_ccrs($RES, $ARG)
+	{
+		$dbc = $this->_container->DBC_User;
+
+		$Lab_Result = new Lab_Result($dbc, $ARG['id']);
+		if (empty($Lab_Result['id'])) {
+			_exit_html_fail('<h1>Lab Result Not Found [CRD-123]</h1>', 400);
+		}
+
+		$Lab_Sample = new Lab_Sample($dbc, $Lab_Result['lab_sample_id']);
+		if (empty($Lab_Sample['id'])) {
+		}
+
+		$data = $this->load_lab_result_full($Lab_Result['id']);
+
+		require_once(APP_ROOT . '/view/pub/ccrs.php');
+
+		// __exit_text('Dump as CSV/CCRS', 501);
 		exit(0);
 	}
+
+	function json($RES, $ARG)
+	{
+		__exit_text('Dump as JSON', 501);
+	}
+
+	/**
+	 * Generate a PDF Linking to this Page
+	 */
+	function pdf($RES, $ARG)
+	{
+		__exit_text('Generate COA?, Download Existing?', 501);
+	}
+
+	/**
+	 * Generate a PDF Linking to this Page
+	 */
+	function png($RES, $ARG)
+	{
+		$qrCode = new \Endroid\QrCode\QrCode(sprintf('https://%s/pub/%s.html', $_SERVER['SERVER_NAME'], $ARG['id']));
+
+		$img_name = sprintf('%s.png', $ARG['id']);
+
+		// Generate a QR Code pointing to this page
+		$RES = $RES->withHeader('content-disposition', sprintf('inline; filename="%s"', $img_name));
+		$RES = $RES->withHeader('content-transfer-encoding', 'binary');
+		$RES = $RES->withHeader('content-type: image/png');
+
+		$RES = $RES->write( $qrCode->writeString() );
+
+		return $RES;
+
+	}
+
+		/**
+	 * Generate a PDF Linking to this Page
+	 */
+	function png_coa($RES, $ARG)
+	{
+		$qrCode = new \Endroid\QrCode\QrCode(sprintf('https://%s/pub/%s.pdf', $_SERVER['SERVER_NAME'], $ARG['id']));
+
+		$img_name = sprintf('%s.png', $ARG['id']);
+
+		// Generate a QR Code pointing to this page
+		header(sprintf('content-disposition: inline; filename="%s"', $img_name));
+		header('content-transfer-encoding: binary');
+		header('content-type: image/png');
+
+		echo $qrCode->writeString();
+
+	}
+
 
 //	function _ouptut_tofile()
 //	{
