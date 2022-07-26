312720b044f21bb450f1bba2806275d06564f5de
openthc <code@openthc.com>
2022-07-26 18:43:22 +0000

    little UOM fix

--- lib/UOM.php
+++ lib/UOM.php
@@ -12,6 +12,7 @@ class UOM
 	public static $uom_list = [
 		'pct' => '%',
 		'mg'  => 'mg',
+		'mg/g' => 'mg/g',
 		'mgg' => 'mg/g',
 		'mgs' => 'mg/s',
 		'aw' => 'a/w',
@@ -26,10 +27,29 @@ class UOM
 	 */
 	static function nice($k)
 	{
+		if (empty($k)) {
+			return '?';
+		}
+
 		$r = self::$uom_list[$k];
 		if (empty($r)) {
 			$r = '-';
 		}
+
+		// switch ($uom) {
+		// 	case 'pct':
+		// 		return '&percnt;';
+		// 	case 'mg_g':
+		// 		return 'mg/g';
+		// 	case 'cfu/g':
+		// 		return 'cfu/g';
+		// 	case 'aw':
+		// 		return 'a<sub>w</sub>';
+		// 	case 'ppm':
+		// 		return 'ppm';
+		// }
+
+		// return '-unknown-';
 
 		return $r;
 	}
