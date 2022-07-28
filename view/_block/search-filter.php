ee033474ec9b17c6b1f6fb4531129a7a15856885
openthc <code@openthc.com>
2022-07-28 20:06:12 +0000

    Improve Search Filter

--- view/_block/search-filter.php
+++ view/_block/search-filter.php
@@ -5,6 +5,10 @@
  * SPDX-License-Identifier: GPL-3.0-only
  */
 
+if (empty($_GET['sort-dir'])) {
+	$_GET['sort-dir'] = 'asc';
+}
+
 if (empty($data['search_field_list'])) {
 	$data['search_field_list'] = [];
 }
@@ -28,16 +32,14 @@ if (empty($data['search_field_list'])) {
 
 <form autocomplete="off">
 
-<input name="sort" type="hidden" value="<?= __h($_GET['sort']) ?>">
-<input name="sort-dir" type="hidden" value="<?= __h($_GET['sort-dir']) ?>">
-
 <div class="lab-result-filter-wrap">
 	<div class="input-group-wrap" style="flex: 1 1 auto; width: 100%;">
 		<div class="input-group">
 			<button class="btn btn-outline-secondary dropdown-toggle"
 				data-bs-toggle="dropdown"
 				id="dd-search-history"
-				type="button" style="border-top-right-radius:0; border-bottom-right-radius:0;"><i class="fas fa-history"></i></button>
+				type="button" style="border-top-right-radius:0; border-bottom-right-radius:0;"><i class="fas fa-history"></i>
+			</button>
 			<div class="dropdown-menu" id="sf-menu-history">
 				...
 			</div>
@@ -65,35 +67,46 @@ if (empty($data['search_field_list'])) {
 			<button class="btn btn-outline-secondary" name="a" type="submit" value="search"><i class="fas fa-search"></i></button>
 		</div>
 	</div>
-	<div class="btn-group" style="margin-left: 1em; min-width: 12em;">
+	<div class="ms-2">
 		<div class="btn-group">
-			<button class="btn btn-outline-secondary dropdown-toggle" data-bs-toggle="dropdown" type="button">Sort Options</button>
-			<ul class="dropdown-menu">
-				<li><button class="dropdown-item" name="sort" value="result-id">Result ID</button></li>
-				<li><button class="dropdown-item" name="sort" value="sample-id">Sample ID</button></li>
-				<!-- <li><button class="dropdown-item" name="sort" value="variety">Variety</button></li> -->
-				<li><button class="dropdown-item" name="sort" value="created-at">Created Date</button></li>
-				<li><button class="dropdown-item" name="sort" value="license-origin">Origin License</button></li>
-			</ul>
+			<div class="btn-group btn-group-sort" style="width: 10em;">
+				<button class="btn btn-outline-secondary dropdown-toggle" data-bs-toggle="dropdown" type="button">Sort Options</button>
+				<ul class="dropdown-menu">
+					<li><button class="dropdown-item" name="sort" type="button" value="result-id">Result ID</button></li>
+					<li><button class="dropdown-item" name="sort" type="button" value="sample-id">Sample ID</button></li>
+					<!-- <li><button class="dropdown-item" name="sort" type="button" value="variety">Variety</button></li> -->
+					<li><button class="dropdown-item" name="sort" type="button" value="created-at">Created Date</button></li>
+					<li><button class="dropdown-item" name="sort" type="button" value="license-origin">Origin License</button></li>
+				</ul>
+			</div>
+			<?php
+			switch (strtolower($_GET['sort-dir'])) {
+				case 'asc':
+				case 'az':
+					echo '<button class="btn btn-outline-secondary btn-sort-dir" name="sort-dir" type="button" value="desc"><i class="fas fa-sort-amount-down"></i></button>';
+					break;
+				case 'desc':
+				case 'za':
+				default:
+					echo '<button class="btn btn-outline-secondary btn-sort-dir" name="sort-dir" type="button" value="asc"><i class="fas fa-sort-amount-up"></i></button>';
+			}
+			?>
 		</div>
-		<?php
-		switch (strtolower($_GET['sort-dir'])) {
-			case 'asc':
-			case 'az':
-				echo '<button class="btn btn-outline-secondary" name="sort-dir" value="desc"><i class="fas fa-sort-amount-down"></i></button>';
-				break;
-			case 'desc':
-			case 'za':
-			default:
-				echo '<button class="btn btn-outline-secondary" name="sort-dir" value="asc"><i class="fas fa-sort-amount-up"></i></button>';
-		}
-		?>
+		<input id="form-sort-key" name="sort" type="hidden" value="<?= __h($_GET['sort']) ?>">
+		<input id="form-sort-dir" name="sort-dir" type="hidden" value="<?= __h($_GET['sort-dir']) ?>">
+	</div>
+	<div class="ms-2">
+		<div class="btn-group">
+			<a class="btn btn-outline-secondary" href="<?= http_build_query(array_merge($_GET, [ 'p' => $data['result_page']['older'] ])) ?>"><i class="fas fa-arrow-left"></i></a>
+			<a class="btn btn-outline-secondary" href="<?= http_build_query(array_merge($_GET, [ 'p' => $data['result_page']['newer'] ])) ?>"><i class="fas fa-arrow-right"></i></a>
+		</div>
 	</div>
 </div>
 </form>
 
 <script>
 $(function() {
+
 	$('#dd-search-history').on('show.bs.dropdown', function() {
 		// console.log(this);
 		var $menu = $(this).parent().find('#sf-menu-history');
@@ -104,7 +117,6 @@ $(function() {
 
 	$('#sf-filter .dropdown-item').on('click', function(e) {
 
-		console.log(e);
 		var txt = e.currentTarget.innerText;
 		// Prepend This To the Input Wrap
 		// Select the Operator of '='
@@ -119,6 +131,27 @@ $(function() {
 		$('#sf-filter-list').append(html.join(''));
 	});
 
+	//
+	$('.btn-group-sort .dropdown-item').on('click', function(e) {
+		var $sel = $(this);
+		var $grp = $sel.closest('.btn-group-sort');
+		var $btn = $grp.find('.btn');
+		$btn.html( $sel.html() );
+		$('#form-sort-key').val ( $sel.val() );
+		$btn.closest('form').submit();
+	});
+
+	$('.btn-sort-dir').on('click', function(e) {
+		var $btn = $(this);
+		$('#form-sort-dir').val ( $btn.val() );
+		$btn.closest('form').submit();
+	});
+
+	// Set the Text to the Right Value
+	var txt = $(`button[value="<?= $_GET['sort'] ?>"]`).text();
+	$('.btn-group-sort .dropdown-toggle').html( txt );
+
+	//
 	$('#lab-result-filter').on('keyup', function() {
 		// Start Filtering the Table?
 		// Ajax Query?
