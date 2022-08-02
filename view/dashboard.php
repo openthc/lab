632d35361c2d978c418f1eac8f7c124a18c85577
openthc <code@openthc.com>
2022-08-02 15:07:27 +0000

    add reports to dashboard

--- view/dashboard.php
+++ view/dashboard.php
@@ -1,8 +1,17 @@
 
+<div class="hero-wrap">
+
+	<div class="hero-core">
+		<h1>Laboratory Data Portal</h1>
+		<!-- <h2>Streamlined interface for <br>Cannabis and Hemp Laboratory Data<br>in BioTrackTHC, LeafData and METRC</h2> -->
+	</div>
+
+</div>
+
 <div class="container mt-2">
 <div class="row card-wrap">
 
-<div class="col-lg-3 col-md-6 mb-4">
+<div class="col-lg-4 col-md-6 mb-4">
   <div class="card">
 	<div class="card-body">
 	  <h4 class="card-title">Samples</h4>
@@ -14,12 +23,11 @@
   </div>
 </div>
 
-<div class="col-lg-3 col-md-6 mb-4">
+<div class="col-lg-4 col-md-6 mb-4">
   <div class="card">
-	<!-- <img class="card-img-top" src="https://placehold.it/500x325" alt=""> -->
 	<div class="card-body">
 	  <h4 class="card-title">Results</h4>
-	  <p class="card-text">Use the <a href="/result">Results</a> page to create or update data for each Sample.</p>
+	  <p class="card-text">Use the <a href="/result">Results</a> page to create or update Result data for each Sample.</p>
 	</div>
 	<div class="card-footer">
 	  <a href="/result" class="btn btn-primary">View Results!</a>
@@ -27,14 +35,17 @@
   </div>
 </div>
 
+<div class="col-lg-4 col-md-6 mb-4">
+  <div class="card">
+	<div class="card-body">
+	  <h4 class="card-title">Reports</h4>
+	  <p class="card-text">Use <a href="/result">Reports</a> to finalise a set of Results and produce documentation.</p>
+	</div>
+	<div class="card-footer">
+	  <a href="/report" class="btn btn-primary">View Reports!</a>
+	</div>
+  </div>
+</div>
+
 </div>
 </div> <!-- /.container -->
-
-<!--
-<div class="container">
-	<iframe border="0" src="https://docs.google.com/document/u/1/d/e/2PACX-1vTC1uQX_KWDo6aGXDKQPIjnJlNDT1G5Zjep60XpAUKiVhegFFtOZki-RuOFTpKvWuvwiM5i0icwzCgX/pub"
-		style="border: none; height: 800px; width:100%;">
-		<p>No IFRAME</p>
-	</iframe>
-</div>
--->
