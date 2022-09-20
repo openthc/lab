ed4688043a1b04bc3218dbffaa6a6e02f76e8ea8
edoceo <code@edoceo.com>
2022-09-20 12:55:19 -0700

    Improve Date Display

--- view/report/single.php
+++ view/report/single.php
@@ -49,19 +49,6 @@ if ( ! empty($data['Lab_Report']['expires_at'])) {
 			);
 		}
 		?></h2>
-		<p>Created: <?= $dtC->format('Y-m-d H:i e') ?>
-		<?php
-		if ( ! empty($data['Lab_Report']['approved_at'])) {
-			$dtA = new \DateTime($data['Lab_Report']['approved_at']);
-			$dtA->setTimezone(new \DateTimezone($_SESSION['tz']));
-			printf('Approved: %s', $dtA->format('Y-m-d H:i e'));
-		}
-		if ( ! empty($data['Lab_Report']['expires_at'])) {
-			$dtE = new \DateTime($data['Lab_Report']['expires_at']);
-			$dtE->setTimezone(new \DateTimezone($_SESSION['tz']));
-			printf('Expires: %s', $dtE->format('Y-m-d H:i e'));
-		}
-		?></p>
 	</div>
 
 	<div>
@@ -158,19 +145,19 @@ if ( ! empty($data['Lab_Report']['expires_at'])) {
 <div class="row">
 	<div class="col-md-4">
 		<div class="input-group">
-			<div class="input-group-prepend"><div class="input-group-text">Created</div></div>
+			<div class="input-group-text">Created</div>
 			<div class="form-control"><?= $dtC_html ?></div>
 		</div>
 	</div>
 	<div class="col-md-4">
 		<div class="input-group">
-			<div class="input-group-prepend"><div class="input-group-text">Approved</div></div>
+			<div class="input-group-text">Approved</div>
 			<div class="form-control"><?= $dtA_html ?></div>
 		</div>
 	</div>
 	<div class="col-md-4">
 		<div class="input-group">
-			<div class="input-group-prepend"><div class="input-group-text">Expires</div></div>
+			<div class="input-group-text">Expires</div>
 			<div class="form-control"><?= $dtE_html ?></div>
 		</div>
 	</div>
