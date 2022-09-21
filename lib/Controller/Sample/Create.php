ae55369c7b1ba013cc23aa4644aa935f7ac516c1
Matt Walther <matt@mashio.net>
2022-09-21 13:38:18 -0700

    Lab Sample Create fixes

--- lib/Controller/Sample/Create.php
+++ lib/Controller/Sample/Create.php
@@ -39,15 +39,42 @@ class Create extends \OpenTHC\Lab\Controller\Base
 			if ( ! empty($data['Source_Lot']['license_id_source'])) {
 				$data['Source_License'] = $dbc->fetchRow('SELECT * FROM license WHERE id = :l0', [ ':l0' => $data['Source_Lot']['license_id_source'] ]);
 			}
-			// __exit_text($data);
 		}
 
 		// POST Handler
 		switch ($_POST['a']) {
 			case 'create-sample':
 
-				$_POST['product'] = trim($_POST['product']);
+				$_POST['product-name'] = trim($_POST['product-name']);
 				$_POST['variety-name'] = trim($_POST['variety-name']);
+
+				if (empty($_POST['license-id'])) {
+					__exit_text('Please Provide a Source License', 400);
+				}
+
+				$Source_License0 = [];
+				$Source_License1 = [];
+
+				$dir = new \OpenTHC\Service\OpenTHC('dir');
+				$res = $dir->get(sprintf('/api/license/%s', $_POST['license-id']));
+				if ( ! empty($res['data']['id'])) {
+					$Source_License0 = $res['data'];
+				}
+
+				$Source_License1 = $dbc->fetchRow('SELECT id, name FROM license WHERE id = :l0', [
+					':l0' => $_POST['license-id']
+				]);
+				if (empty($Source_License1['id'])) {
+					$Source_License1 = [
+						'id' => $Source_License0['id'],
+						'code' => $Source_License0['code'],
+						'guid' => $Source_License0['guid'],
+						'name' => $Source_License0['name'],
+						'type' => $Source_License0['type'],
+						'hash' => '-', // $Source_License0['hash']
+					];
+					$dbc->insert('license', $Source_License1);
+				}
 
 				$dbc->query('BEGIN');
 
@@ -56,57 +83,78 @@ class Create extends \OpenTHC\Lab\Controller\Base
 				$ls['name'] = $_POST['sample-name'];
 				$ls['lot_id'] = $_POST['source-lot-id'];
 				$ls['license_id'] = $_SESSION['License']['id'];
-				$ls['license_id_source'] = $_POST['license-id'];
+				$ls['license_id_source'] = $Source_License1['id'];
 				$ls['qty'] = floatval($_POST['qty']);
 
-				// $P1 = new Product()
 				$P1 = $dbc->fetchRow('SELECT * FROM product WHERE license_id = :l0 AND id = :p0', [
 					':l0' => $_SESSION['License']['id'],
 					':p0' => $_POST['product-id'],
 				]);
 				if (empty($P1['id'])) {
-					// $P1 = [
-					// 	'id' => _ulid(),
-					// 	'license_id' => $_SESSION['License']['id'],
-					// 	'product_type_id' => $_POST['product-type'],
-					// 	'name' => $_POST['product'],
-					// 	'stub' => _text_stub($_POST['product'])
-					// ];
-					// $P1['guid'] = $P1['id'];
-					// $dbc->insert('product', $P1);
+					// Find By Name?
+					$P1 = $dbc->fetchRow('SELECT * FROM product WHERE license_id = :l0 AND name = :p0', [
+						':l0' => $_SESSION['License']['id'],
+						':p0' => $_POST['product-name'],
+					]);
+				}
+				if (empty($P1['id'])) {
+					// __exit_text('Create Product', 500);
+					$P1 = [
+						'id' => _ulid(),
+						'license_id' => $_SESSION['License']['id'],
+						'product_type_id' => $_POST['product-type'],
+						'name' => $_POST['product-name'],
+						'stub' => _text_stub($_POST['product-name'])
+					];
+					$P1['guid'] = $P1['id'];
+					$dbc->insert('product', $P1);
 				}
 				// $ls['product_id'] = $P1['id'];
 
-				$S1 = $dbc->fetchRow('SELECT * FROM variety WHERE license_id = :l0 AND id = :v0', [
+				$V1 = $dbc->fetchRow('SELECT * FROM variety WHERE license_id = :l0 AND id = :v0', [
 					':l0' => $_SESSION['License']['id'],
 					':v0' => $_POST['variety-id'],
 				]);
-				if (empty($S1['id'])) {
-					// $S1 = [
-					// 	'id' => _ulid(),
-					// 	'license_id' => $_SESSION['License']['id'],
-					// 	'name' => $_POST['variety-name'],
-					// ];
-					// $S1['guid'] = $S1['id'];
-					// $dbc->insert('variety', $S1);
+				if (empty($V1['id'])) {
+					$V1 = $dbc->fetchRow('SELECT id, name FROM variety WHERE license_id = :l0 AND name = :p0', [
+						':l0' => $_SESSION['License']['id'],
+						':p0' => $_POST['variety-name']
+					]);
 				}
+				if (empty($V1['id'])) {
+					$V1 = [
+						'id' => _ulid(),
+						'license_id' => $_SESSION['License']['id'],
+						'name' => $_POST['variety-name'],
+					];
+					$V1['guid'] = $V1['id'];
+					$dbc->insert('variety', $V1);
+				}
+				// $ls['variety_id'] = $V1['id'];
 
 				if (empty($ls['lot_id'])) {
-					$ls['lot_id'] = $dbc->insert('inventory', [
-						// 'id' => $ls['id'],
+					$ls['lot_id'] = $ls['id'];
+					$dbc->insert('inventory', [
+						'id' => $ls['lot_id'],
 						'license_id' => $_SESSION['License']['id'],
-						'license_id_source' => $_POST['license-id'],
-						'product_id' => '018NY6XC00PR0DUCT000000000', // $P1['id'],
-						'variety_id' => '018NY6XC00VAR1ETY000000000', // $S1['id'],
+						'license_id_source' => $Source_License1['id'],
+						'product_id' => $P1['id'],
+						'variety_id' => $V1['id'],
 						'section_id' => '018NY6XC00SECT10N000000000',
-						'guid' => $ls['id'],
+						'guid' => $ls['name'],
 						'flag' => (0x00000100 | 0x00000200), // From app/lib/Inventory.php
 						'stat' => 410, // Stat GONE
 						'qty' => 0,
 					]);
+				} else {
+					// Decrement Lot QTY
+					$dbc->query('UPDATE inventory SET qty = qty - :q1 WHERE id = :i0', [
+						':i0' => $ls['lot_id'],
+						':q1' => $ls['qty'],
+					]);
 				}
 
-				$ls->save();
+				$ls->save('Lab_Sample/Create by User');
 
 				$dbc->query('COMMIT');
 
