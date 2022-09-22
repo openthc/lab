d36a15b5a894454301f635c2c69b8352bda64ab6
edoceo <code@edoceo.com>
2022-09-22 11:26:50 -0700

    New UOM List

--- lib/UOM.php
+++ lib/UOM.php
@@ -9,17 +9,66 @@ namespace OpenTHC\Lab;
 
 class UOM
 {
+	/**
+	 * v0 UOM List
+	 */
 	public static $uom_list = [
 		'pct' => '%',
 		'mg'  => 'mg',
-		'mg/g' => 'mg/g',
 		'mgg' => 'mg/g',
+		'mgp' => 'mg/p',
 		'mgs' => 'mg/s',
 		'aw' => 'a/w',
 		'cfu' => 'cfu',
 		'ppm' => 'ppm',
 		'ppb' => 'ppb',
 		'bool' => 'Pass/Fail',
+	];
+
+	/**
+	 * v1 UOM List
+	 */
+	public static $uom_list_2 = [
+		'aw' => [
+			'code' => 'a/w',
+			'name' => '',
+		],
+		'pct' => [
+			'code' => '%',
+			'name' => 'Percent',
+		],
+		'mg' => [
+			'code' => 'mg',
+			'name' => 'Milligrams'
+		],
+		'mgg' => [
+			'code' => 'mg/g',
+			'name' => 'Milligrams per Gram'
+		],
+		'mgp' => [
+			'code' => 'mg/p',
+			'name' => 'Milligrams per Piece',
+		],
+		'mgs' => [
+			'code' => 'mg/s',
+			'name' => 'Milligrams per Serving'
+		],
+		'cfu' => [
+			'code' => 'cfu',
+			'name' => 'Coliform Forming Units',
+		],
+		'ppm' => [
+			'code' => 'ppm',
+			'name' => 'Parts per Million',
+		],
+		'ppb' => [
+			'code' => 'ppb',
+			'name' => 'Parts per Billion',
+		],
+		'bool' => [
+			'code' => 'bool',
+			'name' => 'Pass/Fail'
+		],
 	];
 
 	/**
